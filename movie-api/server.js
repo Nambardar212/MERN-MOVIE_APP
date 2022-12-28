3cee5f60597b37fef81d6ebf2647627c6030bfe7
Nambardar212 <nikhilsaini0210@gmail.com>
2022-12-28 15:33:24 +0530

    Fixed

--- movie-api/server.js
+++ movie-api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/netflix", {
+  .connect("mongodb+srv://movieApp:Chhavi143@cluster0.6kcim8c.mongodb.net/netflix?retryWrites=true", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -24,4 +25,11 @@ app.use("/api/user", userRoutes);
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
+});
+
+
+app.use(express.static(path.resolve("./movie-app-UI/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("./movie-app-UI/build/index.html"));
 });
